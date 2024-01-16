81087661b7fbb995fcf665fe0393a75db554e65b
claramoreira <clara_ljm@hotmail.com>
2024-01-16 20:43:37 -0300

    sprites rescale + removing gravity

--- inimigo.gd
+++ inimigo.gd
@@ -5,10 +5,14 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var tipos_inimigo = $AnimatedSprite2D.frames.get_animation_names()
+	var tipos_inimigo = $AnimatedSprite2D.get_sprite_frames().get_animation_names()
 	$AnimatedSprite2D.animation = tipos_inimigo[randi() % tipos_inimigo.size()]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
