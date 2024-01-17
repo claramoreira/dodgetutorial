d0877681bb09107b1fd286e28a2960efa3adffee
claramoreira <clara_ljm@hotmail.com>
2024-01-16 21:01:48 -0300

    main scene creation

---
+++ principal.gd
@@ -0,0 +1,12 @@
+extends Node
+
+@export var inimigo: PackedScene
+var score
+
+func _ready():
+	randomize()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
